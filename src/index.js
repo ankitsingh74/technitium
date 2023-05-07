e7b9a0895eb5851702ba4124aff0d52ff28a2e8b
Ankit Kumar <ankitroy2292@gmail.com>
2023-05-07 13:54:31 +0530

    commit all file assigment/middleware-1

--- src/index.js
+++ src/index.js
@@ -11,17 +11,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+
+
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+        // console.log("inside GLOBAL MW");
+        //create a middleware using app.use(functionName) so that this piece of code gets called everytime any api is called
 
-app.use('/', route);
+        const assignmentMW = function (req, res, next) {
+            var currentdate = new Date();
+            var datetime = currentdate.getDate() + " "
+                + (currentdate.getMonth() + 1) + " "
+                + currentdate.getFullYear() + " "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+
+            let ip = req.ip
+            let url = req.originalUrl
+            console.log(currentdate);
+            console.log(`${datetime}${ip} ${url}`)
+            next()
+        }
+
+        app.use(assignmentMW)
+        next();
+        app.use('/', route);
+    }
+    
+);
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
